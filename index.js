55449bfc42b6d3a1c2269308a906177fb2d714a7
Jabster28 <justynboyer@gmail.com>
2018-12-03 19:16:32 +0000

    removed trans tracking

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var Jimp = require('jimp');
 require('dotenv').config();
 const io = require('@pm2/io')
 io.init({
-  transactions: true, // will enable the transaction tracing
   metrics: {
     network: {
       ports: true
