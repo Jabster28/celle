d52e3c4de6dbb886690a8c832fb3c45997fc1895
Jabster28 <justynboyer@gmail.com>
2018-12-05 18:56:29 +0000

    debug

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function arrayObjectFind(item, array) {
 
 function findChannel(guld, channelid) {
   for (var i = 0; i < guld.channels.array().length; i++) {
-    channelll = guld.channels.array()[i] 
+    chanelll = guld.channels.array()[i] 
     if (chanelll.id == channelid) {
       return chanelll
     }
