b8a22349927c0375c05b73aa2f59c7b96144333f
Jabster28 <29015942+Jabster28@users.noreply.github.com>
2018-12-22 15:14:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -620,8 +620,10 @@ client.on('message', msg => {
 //!post
 client.on('message', msg => {
   if (isOk(msg) && (msg.member.hasPermission("MANAGE_ROLES") || msg.author.id == 350930610719817728)) {
-    if (msg.content.toLowerCase() == '!post') {
-       embed = new Discord.RichEmbed();
+    text = msg.content.toLowerCase()
+    a = text.split(" ")
+    if (a[0] == "!post") {
+      embed = new Discord.RichEmbed();
       embed.setAuthor(msg.mentions.users.array()[0].username, msg.mentions.users.array()[0].avatarURL)
       embed.setColor("BLUE")
       const yds = msg.attachments.array()[0].proxyURL
